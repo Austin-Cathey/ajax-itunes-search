ca8a3c1e5e8b5dce4cf5d16c5bd8b1af64a76766
Austin-Cathey <austincatheyart@gmail.com>
2023-11-13 10:41:54 -0500

    fetch works and pupulates

--- main.js
+++ main.js
@@ -1,4 +1,49 @@
-const app = {
+
+const musicURL = "https://itunes.apple.com/search?term=";
+let searchForm = document.getElementById("searchForm");
+let searchField = document.getElementById("searchField");
+let button = document.getElementById("searchButton");
+let resultsContainer = document.getElementById("results");
+
+
+
+
+
+    searchForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        let field = []
+         field = searchField.value
+    console.log(field);
+    fetch(musicURL + field + "&media=music").then((response) => {
+        if (response.status === 200) {
+            return response.json();}
+            else {
+                let errorMsg = document.createElement('h2');
+                errorMsg.innerText = "Error, double-check your tunes.";
+                results.appendChild(errorMsg);
+            }
+        }).then((parsedJsonResponse) => {
+            console.log(parsedJsonResponse);
+            const songs = parsedJsonResponse.results;
+            results.innerHTML = []
+            return songs.map(results => {
+                resultsContainer.innerHTML +=
+                `<div class="resultsCard">
+        <img src="${results.artworkUrl30}" class="resultsPhoto">
+        <h3 class="artistName">${results.artistName}</h3>
+        <p class="songTitle">${results.trackName}</p>
+        </div>
+         `
+            })
+            })
+            
+        })
+    
+
+
+    
+    //couldn't get this to work
+        /* const app = {
    data: {
      musicURL: "https://itunes.apple.com/search?term=",
      music: []
@@ -14,7 +59,7 @@ const app = {
     })
     .then(r=> r.json())
     .then(response => {
-        this.data.music = []
+        console.log(response)
         for (let song of response.results) {
             this.data.music.push(song)
         }this.generateMusic();
@@ -48,58 +93,4 @@ main: function() {
 
 },
 }
-app.main()
-/* const musicURL = "https://itunes.apple.com/search?term=";
-let searchForm = document.getElementById("searchForm");
-let searchField = document.getElementById("searchField");
-let button = document.getElementById("searchButton");
-let results = document.getElementById("results");
-const searchResults = {
-    data: {
-        music: []
-    }
-} */
-
-/* let searchForm = document.getElementById("searchForm"),
-    let searchField = document.getElementById("searchField")
-    let field = searchField.value,
-
-
-    searchForm.addEventListener('submit', (event) => {
-        event.preventDefault();
-        let field = searchField.value
-    console.log(field);
-    fetch(musicURL + field).then((response) => {
-        if (response.status === 200) {
-            return response.json();}
-            else {
-                let errorMsg = document.createElement('h2');
-                errorMsg.innerText = "Error, double-check your tunes.";
-                results.appendChild(errorMsg);
-            }
-        }).then((parsedJsonResponse) => {
-            console.log(parsedJsonResponse);
-            
-        })
-    }) */
-
-
-    
-    
-//trying to create resultsCard, not sure how to pull from response
-/*     function resultsInfo() {
-        results.innerHTML = [];
-        for (let songs of searchResults.data.music) {
-            results.innerHTML +=
-         `<div class="resultsCard">
-        <img src="${response.results.artworkUrl30}" class="resultsPhoto">
-        <p class="songTitle">${response.results.trackName}</p>
-        <h3 class="artistName">${response.results.artistName}</h3>
-        </div>
-         `}
-    };
- */
-    /* let allResults = response.map((parsedJsonResponse) =>
-    resultsInfo(response)).join('\n');
-
-    entryPoint.innerHTML = allResults; */
+app.main() */
