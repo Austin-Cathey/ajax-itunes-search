6522827b2b399c0a246f78b436c613b9173d1f97
Austin-Cathey <austincatheyart@gmail.com>
2023-11-16 17:33:58 -0500

    rewrote code, everything works, needs more style

--- main.js
+++ main.js
@@ -24,36 +24,65 @@ let playingPreview = document.getElementById("playingPreview");
                 results.appendChild(errorMsg);
             }
         }).then((parsedJsonResponse) => {
-            /* console.log(parsedJsonResponse); */
+            console.log(parsedJsonResponse);
             const songs = parsedJsonResponse.results;
             results.innerHTML = []
             return songs.map(results => {
-                resultsContainer.innerHTML +=
+                const resultsCard = document.createElement('div');
+                artist = document.createElement('h3');
+                trackList = document.createElement('p');
+                album = document.createElement('img');
+                let playButton = document.createElement('button');
+
+                playButton.innerText = "Play Sample";
+
+                playButton.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    playingPreview.src = results.previewUrl
+                });
+                
+                artist.innerHTML = results.artistName;
+                trackList.innerHTML = results.trackName;
+                album.src = results.artworkUrl100;
+
+                resultsCard.appendChild(album);
+                resultsCard.appendChild(artist);
+                resultsCard.appendChild(trackList);
+                resultsCard.appendChild(playButton);
+                
+                resultsContainer.appendChild(resultsCard);
+                
+                
+                /*    resultsContainer.innerHTML +=
                 `<div class="resultsCard">
         <img src="${results.artworkUrl100}" class="resultsPhoto">
         <h3 class="artistName">${results.artistName}</h3>
         <p class="songTitle">${results.trackName}</p>
-        <button class="playSample" data-id="${results.trackId}">Play Sample</button>
+        <button class="playSample" data-id="${results.reviewUrl}">Play Sample</button>
         </div>
         `
-        
-        let playSample = document.querySelectorAll(".playSample");
-        let previewId = results.trackId
-        
-        //need to use dataset.id for target
-        function play() {
-            playSample.addEventListener('click', (event) => {
-            event.preventDefault();
-                playingPreview.src = results.previewUrl
-                console.log(results.previewUrl)
-                
-        });
-    }
-            });
-        
-            play();
-
-            })
-            
-        })
+         */
+    });
+   
     
+})
+})
+
+/* let playSample = document.querySelectorAll(".playSample");
+function play() {
+    for (let button of playSample)
+    button.addEventListener('click', (event) => {
+event.preventDefault();
+playingPreview.src = event.target.dataset.id
+console.log()    
+})
+}; */
+
+
+
+
+
+
+ 
+ //need to use dataset.id for target
+
